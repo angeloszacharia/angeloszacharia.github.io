64ab4f5ea1d923d2124e064e08227063805c5550
angeloszacharia <151090651+angeloszacharia@users.noreply.github.com>
2025-07-26 18:00:14 +0000

    Deploying to gh-pages from @ angeloszacharia/angeloszacharia.github.io@5d5ed42e136563bd61c079567779e73972eea1d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "See an overview of my academic and professional background here, or download a PDF version.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
