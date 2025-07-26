8a1e80cdbd3afae7c75851f0045302d89b61e625
angeloszacharia <151090651+angeloszacharia@users.noreply.github.com>
2025-07-26 00:26:22 +0000

    Deploying to gh-pages from @ angeloszacharia/angeloszacharia.github.io@7bbad808b392c1d8e90fe5c79314d2796f88de50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "publications",
-          description: "Publications by categories in reversed chronological order.",
+          description: "More details about my publications as well as pre-prints of recent papers can be found in my [Research Gate](https://www.researchgate.net/profile/Angelos-Zacharia) page, [Google Scholar](https://scholar.google.com/citations?user=PvdJyvMAAAAJ&amp;hl=en&amp;oi=ao).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
